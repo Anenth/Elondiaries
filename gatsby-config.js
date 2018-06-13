3dcd6c7a1bb1efee4e373d4cfacbea6d28627637
Anenth <anenthvishnu@gmail.com>
2018-06-13 21:01:27 +0530

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: 'Elon Diaries - A timeline of Elon Musk',
+    description: 'What is Elon talking about today',
+    keywords: 'Elon, Elon Musk, Tesla, SpaceX'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-60526867-3",
+        head: false,
+      },
+    },
+  ],
 }
